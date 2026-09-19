d195fd5b6d8f4776a251b614136f16b837640dc8
Lev <chernishevlev@gmail.com>
2026-09-20 02:12:03 +0300

    perf(sketchybar): use $INFO in volume plugin to eliminate process forks

--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+NAME="${NAME:-volume}"
+
+if [ "$SENDER" = "volume_change" ]; then
+    VOLUME="$INFO"
+    if [ "$VOLUME" = "0" ] || [ -z "$VOLUME" ]; then
+        ICON="󰖁"
+        COLOR="0xff6c7086"
+    else
+        COLOR="0xffcdd6f4"
+        case "$VOLUME" in
+            [6-9][0-9]|100) ICON="󰕾" ;;
+            [3-5][0-9]) ICON="󰖀" ;;
+            *) ICON="󰕿" ;;
+        esac
+    fi
+    sketchybar --set "$NAME" icon="$ICON" label="${VOLUME}%" icon.color="$COLOR"
+    exit 0
+fi
+
 BIN_DIR="$CONFIG_DIR/bin"
 read -r VOLUME MUTED < <("$BIN_DIR/get_volume" 2>/dev/null)
 
@@ -20,5 +39,4 @@ else
     esac
 fi
 
-NAME="${NAME:-volume}"
 sketchybar --set "$NAME" icon="$ICON" label="${VOLUME}%" icon.color="$COLOR"
