c0d19000c7924c3e167a9bd5bc9a23fcd3c2fef0
Lev <chernishevlev@gmail.com>
2026-09-20 21:31:56 +0300

    chore(spotify): remove mouse.scrolled handling from spotify widget

--- sketchybar/plugins/spotify.sh
+++ sketchybar/plugins/spotify.sh
@@ -1,19 +1,5 @@
 #!/bin/zsh
 
-# Handle track skip on scroll
-if [[ "$SENDER" == "mouse.scrolled" ]]; then
-  DELTA="${SCROLL_DELTA:-0}"
-  if [[ "$DELTA" -eq 0 && -n "$INFO" ]]; then
-    DELTA=$(echo "$INFO" | grep -o '"delta": *[-0-9]*' | cut -d: -f2 | tr -d ' ')
-  fi
-  if [[ -n "$DELTA" && "$DELTA" -gt 0 ]]; then
-    osascript /Users/lev/.config/karabiner/scripts/spotify_chrome.scpt next 2>&1 | logger -t sketchybar_spotify
-  elif [[ -n "$DELTA" && "$DELTA" -lt 0 ]]; then
-    osascript /Users/lev/.config/karabiner/scripts/spotify_chrome.scpt previous 2>&1 | logger -t sketchybar_spotify
-  fi
-  exit 0
-fi
-
 SCRIPT_PATH="$CONFIG_DIR/bin/get_spotify.scpt"
 if [[ ! -f "$SCRIPT_PATH" ]]; then
   SCRIPT_PATH="/Users/lev/.config/sketchybar/bin/get_spotify.scpt"
